3e6c8b69c2b580146df75c0b9451f84d61fe5532
Lukas Alexandre <lukas@codelogic.me>
2015-12-31 10:27:19 -0300

    Adding basic Linux support for text-to-speach

--- lib/NATO/say.rb
+++ lib/NATO/say.rb
@@ -1,7 +1,16 @@
+require "os"
+
 module NATO
   module Say
     def self.call(text)
-      `say #{text}`
+      if OS.linux?
+        `echo "#{text}" | espeak`
+      elsif OS.mac?
+        `say #{text}`
+      elsif OS.windows?
+        fail NotImplementedError
+      end
+
       nil
     end
   end
